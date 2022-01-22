d94205c3da6e56134e00f04a342f1cda416d25ff
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-22 00:02:56 -0600

    adding success and failure pages

--- app.js
+++ app.js
@@ -39,6 +39,13 @@ app.post("/", function (req, res) {
         auth: "uje170399@gmail.com:1e58c8ad70d48e654f38308fdc4a6e12-us20"
     }
     const request = https.request(url, options, function (response) {
+
+        if (response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+        }else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+
         response.on("data", function (data) {
             console.log(JSON.parse(data));
         })
@@ -46,6 +53,10 @@ app.post("/", function (req, res) {
     request.write(jsonData);
     request.end();
 
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
