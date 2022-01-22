f20490d7880eb229bd312b6a3c8218a91819dce8
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-21 23:13:05 -0600

    posting data to Mailchimp's Servers

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 //jshint esversion: 6
 
-const express=require("express");
-const bodyParser=require("body-parser");
-const request=require("request");
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
 
 
-const app=express();
+const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html")
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
 });
 
-app.post("/",function(req,res){
-    var firstName=req.body.FName;
-    var lastName=req.body.LName;
-    var email=req.body.email;
+app.post("/", function (req, res) {
+    const firstName = req.body.FName;
+    const lastName = req.body.LName;
+    const email = req.body.email;
 
-    console.log(firstName,lastName,email);
-})
+    var data =
+    {
+        "email_address": email,
+        "status": "subscribed",
+        "merge_fields": {
+            "FNAME": firstName,
+            "LNAME": lastName
+        }
+    }
 
-app.listen(3000,function(){
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us20.api.mailchimp.com/3.0/lists/d8043f7aa6/members";
+    const options = {
+        method: "POST",
+        auth: "uje170399@gmail.com:1e58c8ad70d48e654f38308fdc4a6e12-us20"
+    }
+    const request = https.request(url, options, function (response) {
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    });
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.listen(3000, function () {
     console.log("Server is runnig on port 3000");
 })
